c642ccb9ed3471b67f79a0d5b88db21b81ccce75
Alain Crete <acanuckeh@sympatico.ca>
2015-02-17 21:10:26 -0500

    Added new folder sand finished variables lesson

---
+++ variables/name.rb
@@ -0,0 +1,15 @@
+puts "What is your first name?"
+
+first_name = gets.chomp
+
+puts "What is your last name?"
+
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{last_name}! How are you?"
+
+10.times do
+  puts first_name +" "+ last_name
+end
+
+
