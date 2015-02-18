a104649df5d29373d272618590f4d6cfea79f5b5
Alain Crete <acanuckeh@sympatico.ca>
2015-02-17 21:13:00 -0500

    Deleted old files

--- intro_ex3.rb
+++
@@ -1,7 +0,0 @@
-movies = {:"Jaws" => 1975, :"Spider Man 2" =>2004, :"Gravity" => 2013, :"Shrek" => 2001, :"Chariots of Fire" => 1981 }
-
-puts movies[:"Jaws"]
-puts movies[:"Spider Man 2"]
-puts movies[:"Gravity"]
-puts movies[:"Shrek"]
-puts movies[:"Chariots of Fire"]
